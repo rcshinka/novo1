814f600d86d770a412f2c4df8d47c576b8ce17f8
rcshinka <124839024+rcshinka@users.noreply.github.com>
2023-02-08 23:46:13 +0000

    Segundo código

---
+++ exe2.py
@@ -0,0 +1,6 @@
+nome = input("Insira seu nome")
+endereco = input("Insira seu endereço")
+cep = input("Digite seu cep")
+fone = input("Digite seu telefone")
+
+print( "\n Seu nome: ", nome , "\n Seu endereço: " , endereco , "\nSeu cep: " , cep , "\n Seu número: " ,fone)
